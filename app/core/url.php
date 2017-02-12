f9f445cc0fd0eb32e1cbd3582467be541acc1a2b
Phil Wood <philwood@phils-mbp.home>
2017-02-12 09:01:03 +0000

    Singleton style

--- app/core/url.php
+++ app/core/url.php
@@ -1,7 +1,8 @@
 <?php
 namespace App\Core;
 
-class Url {
+class Url
+{
 
     /**
      * The current full url
@@ -10,6 +11,8 @@ class Url {
      */
     protected $url;
 
+
+
     /**
      * The current url as a key to value array
      * 
@@ -17,80 +20,115 @@ class Url {
      */
     protected $urlArray = array();
 
+
+    public static function Instance()
+    {
+        static $inst = null;
+        if ($inst === null) {
+            $inst = new Url();
+        }
+        return $inst;
+    }
+
+
     /**
      * Constructor
      */
-    public function __construct() {
+    private function __construct()
+    {
         // Initialise properties
         $this->setUrl();
         $this->setUrlArray();
     }
 
+
+
     /**
      * Set the url
      */
-    private function setUrl() {
-        $url  = ( isset( $_SERVER['HTTPS'] ) && 'on' === $_SERVER['HTTPS'] ) ? 'https' : 'http';
-        $url .= '://' . $_SERVER['SERVER_NAME'];
-        $url .= in_array( $_SERVER['SERVER_PORT'], array( '80', '443' ) ) ? '' : ":" . $_SERVER['SERVER_PORT'];
-        $url .= $_SERVER['REQUEST_URI'];
-        $this->url = $url;
+    private function setUrl()
+    {
+        $this->url  = ( isset( $_SERVER['HTTPS'] ) && 'on' === $_SERVER['HTTPS'] ) ? 'https' : 'http';
+        $this->url .= '://' . $_SERVER['SERVER_NAME'];
+        $this->url .= in_array( $_SERVER['SERVER_PORT'], array( '80', '443' ) ) ? '' : ":" . $_SERVER['SERVER_PORT'];
+        $this->url .= $_SERVER['REQUEST_URI'];
     }
+
+
 
     /**
      * Set the url array
      */
-    private function setUrlArray() {
+    private function setUrlArray()
+    {
         $this->urlArray = parse_url($this->url);
         if (isset($this->urlArray['query'])) {
             parse_str(html_entity_decode($this->urlArray['query']), $this->urlArray['query']);
         }
+        $this->urlArray['method'] = $_SERVER['REQUEST_METHOD'];
     }
-    
+
+
+
     /**
      * Get the Url as a string
      * 
      * @return string
      */
-    public function getUrl() : string {
+    public function getUrl() : string
+    {
         return $this->url;
     }
+
+
 
     /**
      * Get the Url as an array
      * 
      * @return array
      */
-    public function geturlArray() : array {
+    public function geturlArray() : array
+    {
         return $this->urlArray;
     }
+
+
 
     /**
      * Get the scheme
      * 
      * @return string
      */
-    public function getScheme() : string {
+    public function getScheme() : string
+    {
         return $this->urlArray['scheme'];
     }
+
+
 
     /**
      * Get the host
      * 
      * @return string
      */
-    public function getHost() : string {
+    public function getHost() : string
+    {
         return $this->urlArray['host'];
     }
+
+
 
     /**
      * Get the paths as an array
      * 
      * @return array
      */
-    public function getPaths() : array {
+    public function getPaths() : array
+    {
         return explode('/', trim($this->urlArray['path'],'/'));
     }
+
+
 
     /**
      * Get the query string as an array
@@ -98,7 +136,20 @@ class Url {
      * 
      * @return array|null
      */
-    public function getQuery() : ?array {
+    public function getQuery() : ?array
+    {
         return $this->urlArray['query'] ?? null;
+    }
+
+
+
+    /**
+     * Get the request method
+     * 
+     * @return string
+     */
+    public function getMethod() : string
+    {
+        return $this->urlArray['method'];
     }
 }
