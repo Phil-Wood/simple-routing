08715f5614ec4b0c71107acd8e43f4ce0cd71457
Phil Wood <philwood@phils-mbp.home>
2017-02-12 09:00:12 +0000

    Http Request Methods Added

--- app/core/routing.php
+++ app/core/routing.php
@@ -3,29 +3,52 @@ namespace App\Core;
 
 use App\Core\Url;
 
-class Routing {
-
+class Routing
+{
 
     /**
      * An array of all set routes
      * 
-     * @var array
+     * @var array $routes
      */
     protected static $routes = array();
 
 
+    /**
+     * Current path array of set route
+     * 
+     * @var array $pathArray
+     */
+    protected static $pathArray = array();
+
 
     /**
-     * The instance of the Url method
+     * Current method of set route
      * 
-     * @var Url $rl
+     * @var array $pathArray
+     */
+    protected static $routeMethod = array();
+
+
+    /**
+     * The instance of the Url object
+     * 
+     * @var Url $url
      */
     protected static $url;
 
 
+    /**
+     * The http request method
+     * 
+     * @var $requestMethod
+     */
+    protected static $requestMethod;
+
 
     /**
      * The error404 callback function
+     * 
      * @var callable $error404
      */
     protected static $error404;
@@ -39,42 +62,111 @@ class Routing {
     protected static $foundMatch;
 
 
+    /**
+     * The key of the matched route
+     * @var int
+     */
+    protected static $matchedKey;
+
 
     /**
      * Set the error404 callback
      * 
      * @param callable $callBack
      */
-    public static function setError404(callable $callBack){
+    public static function setError404(callable $callBack)
+    {
         self::$error404 = $callBack;
     }
 
 
-
     /**
-     * Set a route and associate a callback
-     * 
-     * @param string $route
-     * @param callable $callback
+     * Implement error404 callback
      */
-    public static function set(string $route, callable $callBack) {
-        if(self::checkForDuplicateRoute($route)) {
-            die("A duplicate Route for \"$route\" has been set!");
+    public static function error404()
+    {
+        if (is_callable(self::$error404)) {
+            call_user_func(self::$error404);
+        } else {
+            echo "Not Found!"; // Replace with call to error system
         }
-        self::$routes[] = ['route' => $route, 'callBack' => $callBack];
     }
 
 
 
     /**
-     * Check for duplicatate routes
+     * HTTP method GET
+     * Set a route and associate a callback
      * 
-     * @param  string $route
+     * @param string $path
+     * @param callable $callback
+     */
+    public static function get(string $path, callable $callBack)
+    {
+        self::storeRoute('GET', $path, $callBack);
+    }
+
+
+    /**
+     * HTTP method POST
+     * Set a route and associate a callback
+     * 
+     * @param string $path
+     * @param callable $callback
+     */
+    public static function post(string $path, callable $callBack)
+    {
+        self::storeRoute('POST', $path, $callBack);
+    }
+
+
+    /**
+     * HTTP method DELETE
+     * Set a route and associate a callback
+     * 
+     * @param string $path
+     * @param callable $callback
+     */
+    public static function delete(string $path, callable $callBack)
+    {
+        self::storeRoute('DELETE', $path, $callBack);
+    }
+
+
+    /**
+     * Store the method, path and callback
+     * Check to see if the path has already been set
+     * 
+     * @param  string $method
+     * @param  string $path
+     * @param  callable $callBack
+     */
+    public static function storeRoute(string $method, string $path, callable $callBack)
+    {
+        self::$url = Url::Instance();
+        self::$requestMethod = self::$url->getMethod();
+        self::$routeMethod = $method;
+        self::$pathArray = explode('/', trim($path,'/'));
+
+        if (self::checkForDuplicatePath()) {
+            die("A duplicate Route for \"$path\" has been set!"); // Replace with call to error system
+        } else {
+            self::$routes[] = ['method' => $method, 'path' => self::$pathArray, 'callBack' => $callBack];
+        }
+    }
+
+
+
+    /**
+     * Check for duplicate paths
+     * 
+     * @param  string $method
      * @return bool
      */
-    public static function checkForDuplicateRoute (string $route) : bool {
-        for($i = 0; $i < count(self::$routes); $i++){
-            if(self::$routes[$i]['route'] === $route){
+    public static function checkForDuplicatePath () : bool
+    {
+        for ($i = 0; $i < count(self::$routes); $i++) {
+            if (self::$routes[$i]['path'] === self::$pathArray && self::$routes[$i]['method'] === self::$routeMethod) {
                 return true;
             }
         }
@@ -84,26 +176,52 @@ class Routing {
 
 
     /**
-     * Check if $route matchs a set route and call $callback
+     * Check if current path matchs a set path and implement $callback
      * If no match is found, call self::$error404
      */
-    public static function match() {
+    public static function match()
+    {
+        self::sortRoutes();
 
-        self::$url = new Url();
+        /*
+        echo '<pre>';
+        print_r(self::$routes);
+        echo '</pre>';
+        */
+       
+       echo '<pre>'; print_r(self::$url->getPaths()); echo '</pre>';
 
-        for($i = 0; $i < count(self::$routes); $i++){
-            if(self::$routes[$i]['route'] === '/' . implode('/', self::$url->getPaths())) {
-                self::$routes[$i]['callBack']();
-                self::$foundMatch = true;
+        foreach (self::$routes as $key => $route) {
+            if ($route['path'] === self::$url->getPaths() &&
+                $route['method'] === self::$requestMethod) {
+                    self::$matchedKey = $key;
+                    self::$foundMatch = true;
+                    break;
+            } elseif ($route['path'] === self::$url->getPaths() &&
+                $route['method'] !== self::$requestMethod) {
+                    self::$matchedKey = $key;
+                    self::$foundMatch = true;
             }
         }
-        if(!self::$foundMatch) {
-            if (is_callable(self::$error404)) {
-                call_user_func(self::$error404);
-            }
-            else {
-                echo "Not Found!";
-            }
+
+        if (isset(self::$matchedKey)) {
+            self::$routes[self::$matchedKey]['callBack']();
+        } else {
+            self::error404();
         }
+    }
+
+
+
+    /**
+     * Sort the routes by path count - largest first
+     */
+    public static function sortRoutes()
+    {
+        usort(self::$routes, function ($a, $b) : int {
+            $a = count($a['path']);
+            $b = count($b['path']);
+            return ($a == $b) ? 0 : (($a < $b) ? 1 : - 1);
+        });
     }
 }
